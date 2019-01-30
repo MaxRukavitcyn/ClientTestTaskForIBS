184335b52bb4d54f7e42e4b5661db71b226a8904
MaxHand <rukav77@gmail.com>
2019-01-30 18:39:29 +0500

    start

--- src/app/components/my-component/my-component.component.ts
+++ src/app/components/my-component/my-component.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {MainServiceService} from '../../services/main-service.service';
 import {HttpClient} from '@angular/common/http';
 
 @Component({
@@ -10,15 +9,75 @@ import {HttpClient} from '@angular/common/http';
 export class MyComponentComponent implements OnInit {
   keys: any;
   requisitions: any;
+  obj: object;
+  idReq: number;
+  name: string;
+  emailFrom: string;
+  emailTo: string;
+  status: boolean;
+  isEdit = false;
+  isAdd = false;
 
-  constructor(public service: MainServiceService, public http: HttpClient) {
+  constructor(public http: HttpClient) {
   }
 
   ngOnInit() {
+    this.getRequisition();
+  }
+  getRequisition() {
     this.http.get('http://localhost:8080/test/list').subscribe(
       (res) => {this.requisitions = res;
         this.keys = Object.keys(this.requisitions); }
     );
+  }
+  addRequisition() {
+    const obj = {
+      'id': this.idReq,
+      'name': this.name,
+      'emailFrom': this.emailFrom,
+      'emailTo': this.emailTo,
+      'status': true
+    };
+    console.log(this.obj);
+    this.http.post('http://localhost:8080/test/add', obj
+    ).subscribe((res) => {
+      console.log(res);
+      // alert('Объект добавлен!');
+      this.ngOnInit();
+    });
+  }
+  showEdit() {
+    this.isEdit = !this.isEdit;
+    this.isAdd = false;
+  }
+  showAdd() {
+    this.isAdd = !this.isAdd;
+    this.isEdit = false;
+  }
+  patchRequisition() {
+    const obj = {
+      'id': this.idReq,
+      'name': this.name,
+      'emailFrom': this.emailFrom,
+      'emailTo': this.emailTo,
+      'status': true
+    };
+    console.log(this.obj);
+    this.http.put('http://localhost:8080/test/patch', obj
+    ).subscribe((res) => {
+      console.log(res);
+      // alert('Объект изменен!');
+      this.ngOnInit();
+
+    });
+  }
+  deleteReq(id) {
+    this.http.delete('http://localhost:8080/test/' + id
+    ).subscribe((res) => {
+      console.log(res);
+      this.ngOnInit();
+    });
+
   }
 
 }
--- src/app/services/main-service.service.ts
+++ src/app/services/main-service.service.ts
@@ -1,37 +1,9 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MainServiceService {
-
-  obj: object;
-
-  constructor(private http: HttpClient) {
-  }
-
-  // getRequisitionList(): any {
-  //   // let result: any = null;
-  //   return this.http.get<Object>('http://localhost:8080/test/list').subscribe(
-  //      (data) => {  }
-  //   );
-  //
-  //   // return result;
-  // }
-
-  addRequisition(id, name, emailFrom, emailTo, status) {
-
-    this.obj = {
-      'id': id,
-      'name': name,
-      'emailFrom': emailFrom,
-      'emailTo': emailTo,
-      'status': (status === 'on') ? true : false
-    };
-
-    console.log(this.obj);
-    this.http.post('http://localhost:8080/test/add', this.obj
-    ).subscribe((res) => console.log(res));
+  constructor() {
   }
 }
